c40542eaa58d4cddda8944df9816895a50ef631b
Meet Godhani <meet_godhani1@yahoo.com>
2015-04-27 23:43:32 -0400

    fix

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -1,6 +1,6 @@
 <?php namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Request;
 use Illuminate\Support\Facades\Log;
 use ThreadMeUp\Slack\Client;
 use App\Http\Controllers\Controller;
@@ -10,9 +10,9 @@ class MainController extends Controller {
     public function index()
     {
         $config = [
-            'token' => 'xoxb-4650457057-WggMPzIDpd1DRqscF2KKtx6O',
+            'token' => 'xoxb-4651961255-P4bqtDfrfYnizSlLLRllJ0tn',
             'team' => 'cltron',
-            'username' => 'celertron',
+            'username' => 'meet',
             'icon' => 'ICON', // Auto detects if it's an icon_url or icon_emoji
             'parse' => '', // __construct function in Client.php calls for the parse parameter
         ];
