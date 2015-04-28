64bcd4830889cbd5f1dc7806319c5d89e98bbae2
Meet Godhani <meet_godhani1@yahoo.com>
2015-04-27 23:51:40 -0400

    fix

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -1,6 +1,6 @@
 <?php namespace App\Http\Controllers;
 
-use Request;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use ThreadMeUp\Slack\Client;
 use App\Http\Controllers\Controller;
